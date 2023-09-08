bd5f50e01b1f99de5323b551fc372d0a4764c8cb
Daksh Pandey <dakshpandey127@gmail.com>
2023-09-08 23:05:20 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from "./Navbar";
+import { Route,Routes } from 'react-router-dom';
+import Home from "./Home";
+import Login from "./Login";
+import Signup from "./Signup";
+import Dashboard from './Dashboard';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+  const [isLoggedIn , setIsLoggedIn] =useState(false);
+
+  return(
+     <div>
+      <Navbar isLoggedIn={isLoggedIn}  setIsLoggedIn={setIsLoggedIn}/>
+
+      <Routes>
+        <Route path='/'  element={<Home/>}></Route>
+        <Route path='/login'  element={<Login/>}></Route>
+        <Route path='/signup'  element={<Signup/>}></Route>
+        <Route path='/dashboard'  element={<Dashboard/>}></Route>
+      </Routes>
+     </div>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,63 @@
+import React from "react";
+import logo from "./assets/StudyNotion.png";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { toast } from "react-hot-toast";
+
+const Navbar = (props) => {
+  let isLoggedIn = props.isLoggedIn;
+  let setIsLoggedIn = props.setIsLoggedIn;
+
+  return (
+    <div className="navbar-container">
+      <Link to="/">
+        <img src={logo} className="navbar-logo" alt="Logo" />
+      </Link>
+
+      <nav className="navbar-nav">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+          <li>
+            <Link to="/">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="navbar-buttons">
+        {!isLoggedIn && (
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        )}
+        {!isLoggedIn && (
+          <Link to="/signup">
+            <button>Sign up</button>
+          </Link>
+        )}
+        {isLoggedIn && (
+          <Link to="/">
+            <button
+              onClick={() => {
+                setIsLoggedIn(false);
+                toast.success("Logged Out");
+              }}
+            >
+              Log out
+            </button>
+          </Link>
+        )}
+        {isLoggedIn && (
+          <Link to="/dashboard">
+            <button>Dashboard</button>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
